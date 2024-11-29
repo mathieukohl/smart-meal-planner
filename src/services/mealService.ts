4259020fc28f25cdfc33b8058b4ce2e510a5f087
Mathieu <mathieukohl@hotmail.com>
2024-11-29 16:23:12 +0100

    get the meal details

--- src/services/mealService.ts
+++ src/services/mealService.ts
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-const API_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+const API_BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
 
 export const getMealSuggestions = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`${API_BASE_URL}/filter.php?c=Seafood`);
     return response.data.meals;
   } catch (error) {
     console.error('Error fetching meal suggestions:', error);
     return [];
+  }
+};
+
+export const getMealDetails = async (mealId: string) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/lookup.php?i=${mealId}`);
+    return response.data.meals[0];
+  } catch (error) {
+    console.error('Error fetching meal details:', error);
+    return null;
   }
 };
